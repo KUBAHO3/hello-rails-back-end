aab9e9b0c76f417fd2c87d9e7bc10666a3dc5c75
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-08-18 05:17:12 +0530

    Add routes for the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index] do
+        collection do
+          get :random
+        end
+      end
+    end
+  end
 end
